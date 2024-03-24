8817e95d2370b7fe9972bf458b016df40efa9c83
Lucas <lucas.almdiniz@gmail.com>
2024-03-24 15:17:01 -0400

    feat: :sparkles: Criada função para gerar a lista de seleção de moedas e função para mostrar mensagem de erro

--- src/main.js
+++ src/main.js
@@ -1,1 +1,46 @@
 import './style.css';
+import Swal from 'sweetalert2';
+
+const {API_KEY} = process.env;
+const BASE_URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest`;
+const ID = {
+  selectCurrency: 'select-currency',
+  submitBtn: 'submit-btn',
+  conversionRates: 'conversion-rates',
+};
+
+function messageError(message) {
+  Swal.fire({
+    icon: 'error',
+    title: 'Oops...',
+    text: message,
+  });
+}
+
+function generateCurrencyOption(currency) {
+  const option = document.createElement('option');
+  option.value = currency;
+  option.innerText = currency;
+  return option;
+}
+
+async function generateCurrencySelect() {
+  try {
+    const data = await fetch(`${BASE_URL}/AED`)
+        .then((response) => response.json());
+
+    const currencyList = Object.keys(data.conversion_rates);
+
+    const selectCurrency = document.querySelector(`#${ID.selectCurrency}`);
+    currencyList.forEach((currency) => {
+      selectCurrency.appendChild((generateCurrencyOption(currency)));
+    });
+    selectCurrency.disabled = false;
+  } catch (error) {
+    messageError('Erro ao carregar lista de moedas. Recarregue a página!');
+  }
+}
+
+window.onload = () => {
+  generateCurrencySelect();
+};
