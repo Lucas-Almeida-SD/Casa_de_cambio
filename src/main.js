33034fae00778c94e3ca11820c1d3adb23c2a6d9
Lucas <lucas.almdiniz@gmail.com>
2024-03-25 20:06:53 -0400

    feat: :sparkles: Criada funções generateConversionRateBox e generateConversionRateList
    Função generateConversionRateBox: cria o card da taxa de conversão da moeda; Função generateConversionRateList: realiza a requisição para adquirir a taxa de converção de determinada moeda e gera a lista de cards das taxas de conversões

--- src/main.js
+++ src/main.js
@@ -44,6 +44,60 @@ async function generateCurrencyList() {
   }
 }
 
+function generateConversionRateBox(currency, value) {
+  const conversionRateBox = document.createElement('div');
+  conversionRateBox.classList.add('conversion-rate-box');
+
+  const symbolDiv = document.createElement('div');
+  symbolDiv.classList.add('symbol-box');
+
+  const icon = document.createElement('img');
+  icon.src = './../utils/images/coin.png';
+
+  const currencySpan = document.createElement('span');
+  currencySpan.innerText = currency;
+
+  const valueSpan = document.createElement('span');
+  valueSpan.classList.add('currency-value');
+  valueSpan.innerText = value;
+
+  symbolDiv.appendChild(icon);
+  symbolDiv.appendChild(currencySpan);
+
+  conversionRateBox.appendChild(symbolDiv);
+  conversionRateBox.appendChild(valueSpan);
+  return conversionRateBox;
+}
+
+function generateConversionRateList() {
+  const submitBtn = document.querySelector(`#${ID.submitBtn}`);
+  submitBtn.addEventListener('click', async (event) => {
+    event.preventDefault();
+
+    try {
+      const currencyInput = document.querySelector(`#${ID.currencyInput}`);
+
+      const data = await fetch(`${BASE_URL}/${currencyInput.value}`)
+          .then((response) => response.json());
+      if (data.result === 'error') throw new Error(data['error-type']);
+      const {conversion_rates: conversionRates} = data;
+
+      const conversionRatesDiv = document.querySelector(`#${ID.conversionRates}`);
+      Array.from(conversionRatesDiv.children).forEach((child) => child.remove());
+
+      const conversionRatesTitle = document.querySelector(`#${ID.conversionRatesTitle}`);
+      conversionRatesTitle.innerText = `Valores referentes a 1 ${currencyInput.value}`;
+
+      Object.entries(conversionRates).forEach(([key, value]) => {
+        conversionRatesDiv.appendChild(generateConversionRateBox(key, value));
+      });
+    } catch (error) {
+      messageError('Erro ao pesquisar moeda. Tente novamente!');
+    }
+  });
+}
+
 window.onload = () => {
   generateCurrencyList();
+  generateConversionRateList();
 };
